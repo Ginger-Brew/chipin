f4fc36c9e0d59d55e9eea215cd0a6713ecc361b7
choijingyeong <dldeo9987@gmail.com>
2023-08-23 18:32:22 +0900

    client 배치 수정

--- lib/customer_main/client_menu_unit.dart
+++ lib/customer_main/client_menu_unit.dart
@@ -14,50 +14,41 @@ class ClientMenuUnit extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              flex: 9,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
+      margin: EdgeInsets.fromLTRB(40, 20, 10, 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container(
-                    child: Text(
-                        title,
-                        style: TextStyle(
-                            fontFamily: "Pretendard",
-                            fontSize: 17,
-                            color: Colors.black)
-                    ),
+                  Text(
+                      title,
+                      style: TextStyle(
+                        fontFamily: "Pretendard",
+                        fontSize: 15,
+                        color: Colors.black,)
                   ),
                   Container(
-                    margin: EdgeInsets.only(top: 5.0),
+                    margin: EdgeInsets.only(top: 10),
                     child: Text(
                         prize,
                         style: TextStyle(
-                            fontFamily: "Pretendard",
-                            fontSize: 15,
-                            color: Colors.amber)
+                          fontFamily: "Pretendard",
+                          fontSize: 12,
+                          color: Colors.amber,)
                     ),
                   )
                 ],
               ),
+          Text(
+                count,
+                style: TextStyle(
+                  fontFamily: "Pretendard",
+                  fontSize: 17,
+                  color: Colors.black,)
             ),
-            Expanded(
-              flex: 1,
-              child: Text(
-                  count,
-                  style: TextStyle(
-                      fontFamily: "Pretendard",
-                      fontSize: 20,
-                      color: Colors.black)
-              ),
-            )
-          ],
-        )
+        ],
+      ),
     );
   }
 
